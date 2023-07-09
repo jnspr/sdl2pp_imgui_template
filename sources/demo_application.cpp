3a0d77f1728fe18378e5bf7b91dc8146c621a3bb
Jonas Sprenger <jnspr@outlook.de>
2023-07-10 01:59:58 +0200

    sources/demo_application.cpp: Implement GUI for changing background

--- sources/demo_application.cpp
+++ sources/demo_application.cpp
@@ -5,3 +5,10 @@ DemoApplication::DemoApplication():
     m_bg_color{1.0, 0.0, 0.0}
 {
 }
+
+void DemoApplication::on_gui() {
+    ImGui::Checkbox("Clear background", &m_clear_bg);
+    if (m_clear_bg) {
+        ImGui::ColorPicker3("Background color", m_bg_color);
+    }
+}
