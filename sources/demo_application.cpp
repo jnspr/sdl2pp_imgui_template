ba456d3579f29c75753f435a2041c8e502b95353
Jonas Sprenger <jnspr@outlook.de>
2023-07-10 02:02:28 +0200

    sources/demo_application.cpp: Implement background drawing

--- sources/demo_application.cpp
+++ sources/demo_application.cpp
@@ -6,6 +6,15 @@ DemoApplication::DemoApplication():
 {
 }
 
+void DemoApplication::on_draw() {
+    if (m_clear_bg) {
+        renderer.SetDrawColor(m_bg_color[0] * 255.0,
+                              m_bg_color[1] * 255.0,
+                              m_bg_color[2] * 255.0);
+        renderer.Clear();
+    }
+}
+
 void DemoApplication::on_gui() {
     ImGui::Checkbox("Clear background", &m_clear_bg);
     if (m_clear_bg) {
